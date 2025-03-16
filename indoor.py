874bf02e28d47e289f3e4295c524dac659da3aca
tomneser <101918869+tomneser@users.noreply.github.com>
2025-03-16 06:55:17 +0000

    Add indoor.py

---
+++ indoor.py
@@ -0,0 +1,11 @@
+#In a file called indoor.py, implement a program in Python 
+# that prompts the user for input and then outputs that same input 
+# in lowercase. Punctuation and whitespace should be outputted unchanged.
+# You’re welcome, but not required, to prompt the user explicitly, 
+# as by parsing a str of your own as an argument to input.
+
+# Prompt the user for input
+user_input = input("Please enter some text: ")
+
+# Output the input in lowercase
+print(user_input.lower())
